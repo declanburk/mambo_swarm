b10369243fec2723418119c165f48f426406cf0c
Declan Burke <declan_burke@live.com.au>
2019-11-12 11:26:08 +1100

    Recovering old bearing code

--- scripts/structs.py
+++ scripts/structs.py
@@ -1,3 +1,35 @@
 #!/usr/bin/env python2
 
-two_agents = {'pos': ((0,1,0),(1,0,0)), 'edges': (0,1)}
+# two_agents = {'edges': [['Penne','Rigatoni']], \
+# 			'bearing': ((0,-1,0),) }
+
+two_agents = {'edges': [['Penne','Rigatoni'], \
+						['Penne','Linguine'], \
+						['Penne','Gnocchi'], \
+						['Linguine','Rigatoni'], \
+						['Linguine','Gnocchi'], \
+						['Rigatoni','Gnocchi']], \
+
+			# ## Inverted triangular pyramid
+			# 'bearing': ((2,0,0), \
+			# 			(1,1,0), \
+			# 			(1,0.5,-1), \
+			# 			(1,-1,0), \
+			# 			(0,-0.5,-1), \
+			# 			(-1,0.5,-1)) }
+
+			## Triangular pyramid
+			# 'bearing': ((-2,0,0), \
+			# 			(-1,-1,0), \
+			# 			(-1,-0.5,1), \
+			# 			(-1,1,0), \
+			# 			(0,0.5,1), \
+			# 			(1,-0.5,1)) }
+
+			## Trapozhedra
+			'bearing': ((-2,0,0), \
+						(-1,1,1), \
+						(-1,-1,1), \
+						(-1,-1,-1), \
+						(0,-2,0), \
+						(1,-1,1)) }
